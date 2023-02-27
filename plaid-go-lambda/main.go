a6893b641de4e26d3f7809d90d092ffa62e07f88
srhee34 <60160057+joshrhee@users.noreply.github.com>
2023-02-27 17:21:48 -0500

    Refactored for go-lambda's getAccessToken

--- plaid-go-lambda/main.go
+++ plaid-go-lambda/main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/joshrhee/plaid-go-lambda/CreateLinkToken"
+	"github.com/joshrhee/plaid-go-lambda/GetAccessToken"
 	"log"
-	"net/http"
 	"os"
-	"strings"
 	"sync"
 	"time"
-
-	"github.com/joshrhee/plaid-go-lambda/CreateLinkToken"
-
-	"github.com/aws/aws-lambda-go/events"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -285,13 +281,24 @@ func createLinkToken(c *gin.Context) {
 //  ########################################################################################
 //  ########################################################################################
 
-type transaction struct {
-	Date     string   `json:"date"`
-	Amount   float64  `json:"amount"`
-	Category []string `json:"category"`
-	Name     string   `json:"name"`
+// ########################################################################################
+// ########################################################################################
+// ########################################################################################
+// ########################################################################################
+// ########################################################################################
+// ########################################################################################
+// ########################################################################################
+// ########################################################################################
+func getAccessToken(c *gin.Context) {
+	GetAccessToken.GetAccessToken(c, client, &accessToken, &itemID, &transferID, PLAID_PRODUCTS, FirstDayOfPreviousMonth, LastDayOfPreviousMonth)
 }
 
+//	type transaction struct {
+//		Date     string   `json:"date"`
+//		Amount   float64  `json:"amount"`
+//		Category []string `json:"category"`
+//		Name     string   `json:"name"`
+//	}
 func getTransactionDateRange() {
 	now := time.Now()
 
@@ -301,114 +308,189 @@ func getTransactionDateRange() {
 	LastDayOfPreviousMonth = (time.Date(year, month, 0, 0, 0, 0, 0, now.Location())).String()[0:10]
 }
 
-func RenderError(c *gin.Context, originalErr error) {
-	if plaidError, err := plaid.ToPlaidError(originalErr); err == nil {
-		// Return 200 and allow the front end to render the error.
-		c.JSON(http.StatusOK, gin.H{"error": plaidError})
-		return
-	}
+//
+//func RenderError(c *gin.Context, originalErr error) {
+//	if plaidError, err := plaid.ToPlaidError(originalErr); err == nil {
+//		// Return 200 and allow the front end to render the error.
+//		c.JSON(http.StatusOK, gin.H{"error": plaidError})
+//		return
+//	}
+//
+//	c.JSON(http.StatusInternalServerError, gin.H{"error": originalErr.Error()})
+//}
+//
+//// Getting Access token
+//func getAccessToken(c *gin.Context) {
+//
+//	encodedRequestBody, _ := ioutil.ReadAll(c.Request.Body)
+//	stringPublicTokenObject := string(encodedRequestBody)
+//	// fmt.Println("stringPublicTokenObject: ", stringPublicTokenObject)
+//
+//	splitedString := strings.Split(stringPublicTokenObject, "")
+//
+//	publicTokenArray := []string{}
+//	isEqualFound := false
+//	for i := 0; i < len(splitedString); i++ {
+//		if splitedString[i] == "=" {
+//			isEqualFound = true
+//			continue
+//		}
+//
+//		if !isEqualFound {
+//			continue
+//		}
+//
+//		publicTokenArray = append(publicTokenArray, splitedString[i])
+//	}
+//
+//	publicToken := strings.Join(publicTokenArray, "")
+//
+//	// publicToken := c.Query("public_token")
+//	// uid := c.PostForm("uid")
+//	fmt.Println("publicToken: ", publicToken)
+//	// fmt.Println("uid: ", uid)
+//	if publicToken == "" {
+//		fmt.Println("public token is not exist!!")
+//	}
+//	ctx := context.Background()
+//
+//	// exchange the public_token for an access_token
+//	exchangePublicTokenResp, _, err := client.PlaidApi.ItemPublicTokenExchange(ctx).ItemPublicTokenExchangeRequest(
+//		*plaid.NewItemPublicTokenExchangeRequest(publicToken),
+//	).Execute()
+//	if err != nil {
+//		RenderError(c, err)
+//		return
+//	}
+//
+//	accessToken = exchangePublicTokenResp.GetAccessToken()
+//
+//	// Add DynamoDB for {clientUserID: accessToken}
+//	//putDynamoDB(clientUserId)
+//
+//	itemID = exchangePublicTokenResp.GetItemId()
+//	if itemExists(strings.Split(PLAID_PRODUCTS, ","), "transfer") {
+//		transferID, err = authorizeAndCreateTransfer(ctx, client, accessToken)
+//	}
+//
+//	// Getting Transaction
+//	transactionRequest := plaid.NewTransactionsGetRequest(
+//		accessToken,
+//		FirstDayOfPreviousMonth,
+//		LastDayOfPreviousMonth,
+//	)
+//
+//	fmt.Println("TransactionRequest: ", *transactionRequest)
+//
+//	options := plaid.TransactionsGetRequestOptions{
+//		Count:  plaid.PtrInt32(100),
+//		Offset: plaid.PtrInt32(0),
+//	}
+//
+//	transactionRequest.SetOptions(options)
+//
+//	fmt.Println("After SetOptions, TransactionRequest: ", *transactionRequest)
+//
+//	transactionResponse, _, err := client.PlaidApi.TransactionsGet(ctx).TransactionsGetRequest(*transactionRequest).Execute()
+//	if err != nil {
+//		fmt.Errorf("Transaction get error: ", err)
+//	}
+//
+//	fmt.Println("transactionResponse: ", transactionResponse)
+//
+//	var editedTransactions []transaction
+//	transactions := transactionResponse.GetTransactions()
+//
+//	fmt.Println("transactions: ", transactions)
+//	fmt.Println("transactionResponse.TotalTransactions: ", transactionResponse.TotalTransactions)
+//
+//	for i := 0; i < int(transactionResponse.TotalTransactions); i++ {
+//		editedTransaction := transaction{
+//			Date:     transactions[i].Date,
+//			Amount:   transactions[i].Amount,
+//			Category: transactions[i].Category,
+//			Name:     transactions[i].Name,
+//		}
+//		editedTransactions = append(editedTransactions, editedTransaction)
+//	}
+//
+//	fmt.Println("editedTransactions: ", editedTransactions)
+//	c.JSON(http.StatusOK, editedTransactions)
+//}
 
-	c.JSON(http.StatusInternalServerError, gin.H{"error": originalErr.Error()})
-}
+//// This is a helper function to authorize and create a Transfer after successful
+//// exchange of a public_token for an access_token. The transfer_id is then used
+//// to obtain the data about that particular Transfer.
+//func authorizeAndCreateTransfer(ctx context.Context, client *plaid.APIClient, accessToken string) (string, error) {
+//	// We call /accounts/get to obtain first account_id - in production,
+//	// account_id's should be persisted in a data store and retrieved
+//	// from there.
+//	accountsGetResp, _, _ := client.PlaidApi.AccountsGet(ctx).AccountsGetRequest(
+//		*plaid.NewAccountsGetRequest(accessToken),
+//	).Execute()
+//
+//	accountID := accountsGetResp.GetAccounts()[0].AccountId
+//
+//	transferAuthorizationCreateUser := plaid.NewTransferUserInRequest("FirstName LastName")
+//	transferAuthorizationCreateRequest := plaid.NewTransferAuthorizationCreateRequest(
+//		accessToken,
+//		accountID,
+//		"credit",
+//		"ach",
+//		"1.34",
+//		"ppd",
+//		*transferAuthorizationCreateUser,
+//	)
+//	transferAuthorizationCreateResp, _, err := client.PlaidApi.TransferAuthorizationCreate(ctx).TransferAuthorizationCreateRequest(*transferAuthorizationCreateRequest).Execute()
+//	if err != nil {
+//		return "", err
+//	}
+//	authorizationID := transferAuthorizationCreateResp.GetAuthorization().Id
+//
+//	transferCreateRequest := plaid.NewTransferCreateRequest(
+//		accessToken,
+//		accountID,
+//		authorizationID,
+//		"credit",
+//		"ach",
+//		"1.34",
+//		"Payment",
+//		"ppd",
+//		*transferAuthorizationCreateUser,
+//	)
+//	transferCreateResp, _, err := client.PlaidApi.TransferCreate(ctx).TransferCreateRequest(*transferCreateRequest).Execute()
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	return transferCreateResp.GetTransfer().Id, nil
+//}
 
-// Getting Access token
-func getAccessToken(c *gin.Context) {
+//// Helper function to determine if Transfer is in Plaid product array
+//func itemExists(array []string, product string) bool {
+//	for _, item := range array {
+//		if item == product {
+//			return true
+//		}
+//	}
+//
+//	return false
+//}
 
-	encodedRequestBody, _ := ioutil.ReadAll(c.Request.Body)
-	stringPublicTokenObject := string(encodedRequestBody)
-	// fmt.Println("stringPublicTokenObject: ", stringPublicTokenObject)
-
-	splitedString := strings.Split(stringPublicTokenObject, "")
-
-	publicTokenArray := []string{}
-	isEqualFound := false
-	for i := 0; i < len(splitedString); i++ {
-		if splitedString[i] == "=" {
-			isEqualFound = true
-			continue
-		}
-
-		if !isEqualFound {
-			continue
-		}
-
-		publicTokenArray = append(publicTokenArray, splitedString[i])
-	}
-
-	publicToken := strings.Join(publicTokenArray, "")
-
-	// publicToken := c.Query("public_token")
-	// uid := c.PostForm("uid")
-	fmt.Println("publicToken: ", publicToken)
-	// fmt.Println("uid: ", uid)
-	if publicToken == "" {
-		fmt.Println("public token is not exist!!")
-	}
-	ctx := context.Background()
-
-	// exchange the public_token for an access_token
-	exchangePublicTokenResp, _, err := client.PlaidApi.ItemPublicTokenExchange(ctx).ItemPublicTokenExchangeRequest(
-		*plaid.NewItemPublicTokenExchangeRequest(publicToken),
-	).Execute()
-	if err != nil {
-		RenderError(c, err)
-		return
-	}
-
-	accessToken = exchangePublicTokenResp.GetAccessToken()
-
-	// Add DynamoDB for {clientUserID: accessToken}
-	//putDynamoDB(clientUserId)
-
-	itemID = exchangePublicTokenResp.GetItemId()
-	if itemExists(strings.Split(PLAID_PRODUCTS, ","), "transfer") {
-		transferID, err = authorizeAndCreateTransfer(ctx, client, accessToken)
-	}
-
-	// Getting Transaction
-	transactionRequest := plaid.NewTransactionsGetRequest(
-		accessToken,
-		FirstDayOfPreviousMonth,
-		LastDayOfPreviousMonth,
-	)
-
-	fmt.Println("TransactionRequest: ", *transactionRequest)
-
-	options := plaid.TransactionsGetRequestOptions{
-		Count:  plaid.PtrInt32(100),
-		Offset: plaid.PtrInt32(0),
-	}
-
-	transactionRequest.SetOptions(options)
-
-	fmt.Println("After SetOptions, TransactionRequest: ", *transactionRequest)
-
-	transactionResponse, _, err := client.PlaidApi.TransactionsGet(ctx).TransactionsGetRequest(*transactionRequest).Execute()
-	if err != nil {
-		fmt.Errorf("Transaction get error: ", err)
-	}
-
-	fmt.Println("transactionResponse: ", transactionResponse)
-
-	var editedTransactions []transaction
-	transactions := transactionResponse.GetTransactions()
-
-	fmt.Println("transactions: ", transactions)
-	fmt.Println("transactionResponse.TotalTransactions: ", transactionResponse.TotalTransactions)
-
-	for i := 0; i < int(transactionResponse.TotalTransactions); i++ {
-		editedTransaction := transaction{
-			Date:     transactions[i].Date,
-			Amount:   transactions[i].Amount,
-			Category: transactions[i].Category,
-			Name:     transactions[i].Name,
-		}
-		editedTransactions = append(editedTransactions, editedTransaction)
-	}
-
-	fmt.Println("editedTransactions: ", editedTransactions)
-	c.JSON(http.StatusOK, editedTransactions)
-}
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
+//  ########################################################################################
 
 // func SendMessageToSQS(queueUrl string, messages []string) error {
 //     // Load the AWS SDK configuration
@@ -435,65 +517,6 @@ func getAccessToken(c *gin.Context) {
 
 //     return nil
 // }
-
-// This is a helper function to authorize and create a Transfer after successful
-// exchange of a public_token for an access_token. The transfer_id is then used
-// to obtain the data about that particular Transfer.
-func authorizeAndCreateTransfer(ctx context.Context, client *plaid.APIClient, accessToken string) (string, error) {
-	// We call /accounts/get to obtain first account_id - in production,
-	// account_id's should be persisted in a data store and retrieved
-	// from there.
-	accountsGetResp, _, _ := client.PlaidApi.AccountsGet(ctx).AccountsGetRequest(
-		*plaid.NewAccountsGetRequest(accessToken),
-	).Execute()
-
-	accountID := accountsGetResp.GetAccounts()[0].AccountId
-
-	transferAuthorizationCreateUser := plaid.NewTransferUserInRequest("FirstName LastName")
-	transferAuthorizationCreateRequest := plaid.NewTransferAuthorizationCreateRequest(
-		accessToken,
-		accountID,
-		"credit",
-		"ach",
-		"1.34",
-		"ppd",
-		*transferAuthorizationCreateUser,
-	)
-	transferAuthorizationCreateResp, _, err := client.PlaidApi.TransferAuthorizationCreate(ctx).TransferAuthorizationCreateRequest(*transferAuthorizationCreateRequest).Execute()
-	if err != nil {
-		return "", err
-	}
-	authorizationID := transferAuthorizationCreateResp.GetAuthorization().Id
-
-	transferCreateRequest := plaid.NewTransferCreateRequest(
-		accessToken,
-		accountID,
-		authorizationID,
-		"credit",
-		"ach",
-		"1.34",
-		"Payment",
-		"ppd",
-		*transferAuthorizationCreateUser,
-	)
-	transferCreateResp, _, err := client.PlaidApi.TransferCreate(ctx).TransferCreateRequest(*transferCreateRequest).Execute()
-	if err != nil {
-		return "", err
-	}
-
-	return transferCreateResp.GetTransfer().Id, nil
-}
-
-// Helper function to determine if Transfer is in Plaid product array
-func itemExists(array []string, product string) bool {
-	for _, item := range array {
-		if item == product {
-			return true
-		}
-	}
-
-	return false
-}
 
 // Get DynamoDB
 func GetDynamoDB() *dynamodb.Client {
